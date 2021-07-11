adb575f1fd79769dff32c6de748d142d57b91f58
Aram <example@gmail.com>
2021-07-11 08:13:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//alert("Hello World!");
+//let answer = confirm("Are you here?");
+//console.log(answer);
+
+//let answer = prompt("are you 16?", "yes");
+//console.log(answer);
+
+let incr = 10;
+let decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+console.log(incr + "2");
+console.log(typeof(incr));
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = true;
+
+    console.log(isChecked || isClose);
