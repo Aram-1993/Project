2d1084d4d26d2792c8367b42f4ff1c730c746f04
Aram <example@gmail.com>
2021-08-15 16:32:05 +0200

    Add a form

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -34,7 +34,7 @@ info.addEventListener('click', function(event) {
 });
 
 //Timer
-let deadline = '2021.08.10';
+let deadline = '2021.09.10';
 
 function getTimeRemainig(endtime) {
    let t = Date.parse(endtime) - Date.parse(new Date()),
@@ -86,37 +86,66 @@ setClock('timer', deadline);
 
 //Window1
 
+// Modal
+
 let more = document.querySelector('.more'),
-    overlay = document.querySelector('.overlay'),
-    close = document.querySelector('.popup-close');
+overlay = document.querySelector('.overlay'),
+close = document.querySelector('.popup-close');
 
-    more.addEventListener('click', function() {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';
+more.addEventListener('click', function() {
+overlay.style.display = 'block';
+this.classList.add('more-splash');
+document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click', function() {
+overlay.style.display = 'none';
+more.classList.remove('more-splash');
+document.body.style.overflow = '';
+});
+
+
+// Send a form
+
+let message = {
+    loading: "Идёт загрузка...",
+    success: "Спасибо вам! Мы с вами свяжемся.",
+    failure: "Что-то пошло не так..."
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+       event.preventDefault();
+       form.appendChild(statusMessage);
+
+       let request = new XMLHttpRequest();
+       request.open('POST', 'server.php');
+       request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+       
+    let formData = new FormData(form);
+      request.send(formData);
+    
+      request.addEventListener('readystatechange', function() {
+     if(request.readyState < 4) {
+         statusMessage.innerHTML = message.loading;
+     }else if(request.readyState === 4 && request.status == 200) {
+        statusMessage.innerHTML = message.success;  
+     }else {
+        statusMessage.innerHTML = message.failure;  
+     }
+
     });
 
-    close.addEventListener('click', function(){
-        overlay.style.display = 'none';
-        more.classList.remove('more-splash');
-        document.body.style.overflow = '';
+     for(let i = 0; i < input.length; i++) {
+         input[i].value = '';
+     }
+    
     });
-
-    //Window2
-
-    let descriptionBtn = document.querySelectorAll('.description-btn')[0],
-        priceSum = document.getElementsByClassName('counter');
-        
-
-        descriptionBtn.addEventListener('click', function(){
-           content.counter.style.display = 'block';
-           this.classList.add('content-splash');
-           document.body.style.overflow = 'hidden';
-
-        });
-         
-        console.log(descriptionBtn);
-        console.log(priceSum);
 
 });
 
--- yoga/server.php
+++ yoga/server.php
@@ -1,3 +1,6 @@
 <?php 
 
-echo $_SERVER[‘REMOTE_ADDR‘];
+echo $_SERVER['REMOTE_ADDR'];
+
+?>
+
